4856f8d2aff1905c535997e7a6d3fc175467da32
Aariz Iqbal <aariz.iqbal@outlook.com>
2024-02-17 23:59:42 -0800

    Added the hover effect with the underline

--- frontend/portfolio/app/_components/Navbar.tsx
+++ frontend/portfolio/app/_components/Navbar.tsx
@@ -3,19 +3,28 @@ import Link from "next/link";
 const Navbar = () => {
 
     const listLinkStyling = `ml-20 text-white opacity-50 
-                            hover:opacity-100 `
+                            hover:opacity-100 ease-in-out relative group`
+    
+    const underLine = <div className="pt-0.5 absolute w-full h-0.5 bg-white/80 scale-x-0 group-hover:scale-x-100 transition-transform origin-left"/>
     
 
     return (
 
         <div className="items-end">
-            
-                <ul className=" flex list-none mt-8 justify-end">
-                    <li className={listLinkStyling}>Experience</li>
-                    <li className={listLinkStyling}>Projects</li>
-                    <li className={`${listLinkStyling} mr-14`}>Contacts</li>
-                </ul>
-                
+            <ul className=" flex list-none mt-8 justify-end">
+                <li className={listLinkStyling}>
+                    Experience
+                    {underLine}
+                </li>
+                <li className={listLinkStyling}>
+                    Projects
+                    {underLine}
+                </li>
+                <li className={`${listLinkStyling} mr-14`}>
+                    Contacts
+                    {underLine}
+                </li>
+            </ul> 
         </div>
     )
 }
